525bb552637ea2be7a41f939ebc6f216bed6f213
TruongDepZai <nguyenxuantruong96.hust@gmail.com>
2018-12-16 20:43:46 +0700

    Cấu hình SQL

---
+++ src/main/java/com/codegym/managerprocesssale/ManagerProcessSaleApplication.java
@@ -0,0 +1,14 @@
+package com.codegym.managerprocesssale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerProcessSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerProcessSaleApplication.class, args);
+	}
+
+}
+
