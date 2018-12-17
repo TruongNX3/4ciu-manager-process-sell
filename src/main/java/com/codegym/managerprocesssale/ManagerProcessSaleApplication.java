61396ca3ced3257ccc780860aadd66895ceaeade
HaTien <havantien95tb@gmail.com>
2018-12-17 10:43:00 +0700

    Thêm chức năng quản lý sản phẩm

--- src/main/java/com/codegym/managerprocesssale/ManagerProcessSaleApplication.java
+++ src/main/java/com/codegym/managerprocesssale/ManagerProcessSaleApplication.java
@@ -1,10 +1,17 @@
 package com.codegym.managerprocesssale;
 
+import com.codegym.managerprocesssale.service.ProductService;
+import com.codegym.managerprocesssale.service.impl.ProductServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ManagerProcessSaleApplication {
+	@Bean
+	public ProductService productService() {
+		return new ProductServiceImpl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerProcessSaleApplication.class, args);
