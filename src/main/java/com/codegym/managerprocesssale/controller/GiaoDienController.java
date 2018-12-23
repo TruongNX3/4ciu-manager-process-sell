0c9a9ad70a91797afa224e945fa11fd39baec1d9
HaTien <havantien95tb@gmail.com>
2018-12-23 14:09:43 +0700

    Giao Diện Dự Án

---
+++ src/main/java/com/codegym/managerprocesssale/controller/GiaoDienController.java
@@ -0,0 +1,13 @@
+package com.codegym.managerprocesssale.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GiaoDienController {
+    @GetMapping("index")
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+}
